5839a0e97ce5b614c5a50d62cedea1e520fb8f3a
jdelgado <jdelgado.berm@gmail.com>
2024-01-28 07:23:08 +0100

    feat: exported class Room to his own file

--- backend/room.js
+++ backend/room.js
@@ -1,9 +1,10 @@
-class Room{
+class Room {
 
-	constructor(roomID){
+	constructor(roomID) {
 		this.roomID = roomID;
 		this.usersReady = 0;
 		this.selectionsReady = 0;
 		this.rounds = 0;
 	}
 }
+global.Room = Room;
